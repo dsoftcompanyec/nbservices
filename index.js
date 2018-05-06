60997be50c9ef83d245653daf7bfa7e28a0ca728
Javier Lugmania <jlugmania@gmail.com>
2018-05-06 11:19:37 -0500

    Se corrige caga de dependencias

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
-import http from "http";
+let express = require("express");
+let bodyParser = require("body-parser");
 
 const server = express();
 
